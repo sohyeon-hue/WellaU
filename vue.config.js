1b5a634967df2b7bbd4dcba4ccdf351caed67485
sohyeon.lee <sohyun.lee@inno-driven.com>
2025-06-19 09:07:23 +0900

    commit

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: 'WellaU',
+  publicPath: '/WellaU/',
   outputDir: './docs',
   chainWebpack: config => {
     config.resolve.alias
